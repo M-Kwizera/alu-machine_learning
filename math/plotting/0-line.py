945423af97d1c3471470b0426f2045544b732358
M-Kwizera <122270470+M-Kwizera@users.noreply.github.com>
2024-05-13 00:17:34 +0300

    Update 0-line.py
    Added code to the already existing python file

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,1 +1,9 @@
-A
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = np.arange(0, 11) ** 3
+
+plt.plot(y, color='red')  # Plotting y as a solid red line
+plt.xlim(0, 10)  # Setting x-axis range from 0 to 10
+plt.show()
