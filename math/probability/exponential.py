92eca6f605417bbd40d94b5760b99675491fcb7a
M-Kwizera <122270470+M-Kwizera@users.noreply.github.com>
2024-06-10 14:23:27 +0200

    made major  changes to exponential.py

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """
-Representing Poisson distribution
+Representing exponential distribution
 """
 
-import math
 
 class Exponential:
     """
-    Class Poisson distribution
+    Class exponential distribution
     """
+    e = 2.7182818285
 
     def __init__(self, data=None, lambtha=1.):
         """
-        Class constructor
+        Class contructor
         """
         if data is None:
             if lambtha <= 0:
@@ -24,46 +24,22 @@ class Exponential:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
+            self.lambtha = 1 / (sum(data) / len(data))
 
-    def pmf(self, k):
+    def pdf(self, x):
         """
-        Probability Mass Function for Poisson
+        Probability Density Function for exponential
         """
-        try:
-            k = int(k)
-        except (ValueError, TypeError):
+        if x < 0:
             return 0
-        if k < 0:
-            return 0
-        pmf = (self.lambtha ** k * math.exp(-self.lambtha)) / math.factorial(k)
-        return pmf
+        pdf = self.lambtha * Exponential.e ** - (self.lambtha * x)
+        return pdf
 
-    def cdf(self, k):
+    def cdf(self, x):
         """
-        Cumulative Distribution Function for Poisson
+        Cumulative Distribution Function for exponential
         """
-        try:
-            k = int(k)
-        except (ValueError, TypeError):
+        if x < 0:
             return 0
-        if k < 0:
-            return 0
-        cdf = sum((self.lambtha ** i * math.exp(-self.lambtha)) / math.factorial(i) for i in range(k + 1))
+        cdf = 1 - Exponential.e ** (- self.lambtha * x)
         return cdf
-
-# Test script
-if __name__ == "__main__":
-    import numpy as np
-
-    np.random.seed(0)
-    data = np.random.poisson(5., 100).tolist()
-    p1 = Exponential(data)
-    print('Lambtha:', p1.lambtha)
-    print('PMF for k=3:', p1.pmf(3))
-    print('CDF for k=3:', p1.cdf(3))
-
-    p2 = Exponential(lambtha=5)
-    print('Lambtha:', p2.lambtha)
-    print('PMF for k=3:', p2.pmf(3))
-    print('CDF for k=3:', p2.cdf(3))
